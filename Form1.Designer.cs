a682c299f42fb17f871f42848c0fbe2f65c03039
marinaoleinik <marina.oleinik@tthk.ee>
2024-11-20 11:33:34 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Andmebaas_TARpv23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Nimetus_txt = new System.Windows.Forms.TextBox();
+            this.Kogus_txt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Hind_txt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lisa_btn = new System.Windows.Forms.Button();
+            this.uuenda_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.otsipilt_btn = new System.Windows.Forms.Button();
+            this.kustuta_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(31, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nimetus";
+            // 
+            // Nimetus_txt
+            // 
+            this.Nimetus_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Nimetus_txt.Location = new System.Drawing.Point(139, 69);
+            this.Nimetus_txt.Name = "Nimetus_txt";
+            this.Nimetus_txt.Size = new System.Drawing.Size(100, 32);
+            this.Nimetus_txt.TabIndex = 1;
+            // 
+            // Kogus_txt
+            // 
+            this.Kogus_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Kogus_txt.Location = new System.Drawing.Point(139, 118);
+            this.Kogus_txt.Name = "Kogus_txt";
+            this.Kogus_txt.Size = new System.Drawing.Size(100, 32);
+            this.Kogus_txt.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label2.Location = new System.Drawing.Point(31, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 26);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kogus";
+            // 
+            // Hind_txt
+            // 
+            this.Hind_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.Hind_txt.Location = new System.Drawing.Point(139, 171);
+            this.Hind_txt.Name = "Hind_txt";
+            this.Hind_txt.Size = new System.Drawing.Size(100, 32);
+            this.Hind_txt.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label3.Location = new System.Drawing.Point(31, 177);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 26);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Hind";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(36, 276);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(634, 150);
+            this.dataGridView1.TabIndex = 6;
+            this.dataGridView1.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseEnter);
+            this.dataGridView1.CellMouseLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellMouseLeave);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // lisa_btn
+            // 
+            this.lisa_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.lisa_btn.Location = new System.Drawing.Point(36, 222);
+            this.lisa_btn.Name = "lisa_btn";
+            this.lisa_btn.Size = new System.Drawing.Size(151, 38);
+            this.lisa_btn.TabIndex = 7;
+            this.lisa_btn.Text = "Lisa andmed";
+            this.lisa_btn.UseVisualStyleBackColor = true;
+            this.lisa_btn.Click += new System.EventHandler(this.lisa_btn_Click);
+            // 
+            // uuenda_btn
+            // 
+            this.uuenda_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.uuenda_btn.Location = new System.Drawing.Point(193, 222);
+            this.uuenda_btn.Name = "uuenda_btn";
+            this.uuenda_btn.Size = new System.Drawing.Size(151, 38);
+            this.uuenda_btn.TabIndex = 8;
+            this.uuenda_btn.Text = "Uuenda andmed";
+            this.uuenda_btn.UseVisualStyleBackColor = true;
+            this.uuenda_btn.Click += new System.EventHandler(this.uuenda_btn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(281, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(241, 139);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // otsipilt_btn
+            // 
+            this.otsipilt_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.otsipilt_btn.Location = new System.Drawing.Point(507, 222);
+            this.otsipilt_btn.Name = "otsipilt_btn";
+            this.otsipilt_btn.Size = new System.Drawing.Size(151, 38);
+            this.otsipilt_btn.TabIndex = 10;
+            this.otsipilt_btn.Text = "Pildi otsing";
+            this.otsipilt_btn.UseVisualStyleBackColor = true;
+            this.otsipilt_btn.Click += new System.EventHandler(this.otsipilt_btn_Click);
+            // 
+            // kustuta_btn
+            // 
+            this.kustuta_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.kustuta_btn.Location = new System.Drawing.Point(350, 222);
+            this.kustuta_btn.Name = "kustuta_btn";
+            this.kustuta_btn.Size = new System.Drawing.Size(151, 38);
+            this.kustuta_btn.TabIndex = 11;
+            this.kustuta_btn.Text = "Kustuta andmed";
+            this.kustuta_btn.UseVisualStyleBackColor = true;
+            this.kustuta_btn.Click += new System.EventHandler(this.kustuta_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.kustuta_btn);
+            this.Controls.Add(this.otsipilt_btn);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.uuenda_btn);
+            this.Controls.Add(this.lisa_btn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Hind_txt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Kogus_txt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Nimetus_txt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Ladu";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Nimetus_txt;
+        private System.Windows.Forms.TextBox Kogus_txt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Hind_txt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button lisa_btn;
+        private System.Windows.Forms.Button uuenda_btn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button otsipilt_btn;
+        private System.Windows.Forms.Button kustuta_btn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Andmebaas_TARpv23
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\marina.oleinik\source\repos\Andmebaas_TARpv23\Database.mdf");
+
+        //SqlConnection conn = new SqlConnection(ConfigurationSettings.AppSettings["AndmebaasConnectionString"]);
+        
+        SqlCommand cmd;
+        SqlDataAdapter adapter;
+        OpenFileDialog open;
+        SaveFileDialog save;
+        FileInfo openfile;
+        string extension;
+        byte[] imageData;
+        public Form1()
+        {
+            InitializeComponent();
+            NaitaAndmed();
+        }
+
+        public void NaitaAndmed()
+        {
+            conn.Open();
+            DataTable dt = new DataTable();
+            cmd = new SqlCommand("SELECT * FROM Toode",conn);
+            adapter=new SqlDataAdapter(cmd);
+            adapter.Fill(dt);
+            dataGridView1.DataSource = dt;
+            conn.Close();
+        }
+
+        private void lisa_btn_Click(object sender, EventArgs e)
+        {
+            if (Nimetus_txt.Text.Trim() != string.Empty && Kogus_txt.Text.Trim() != string.Empty && Hind_txt.Text.Trim() != string.Empty )
+            {
+                try
+                {
+                    conn.Open();
+                    cmd = new SqlCommand("INSERT INTO Toode(Nimetus,Kogus,Hind,Pilt,FusPilt) VALUES (@toode,@kogus,@hind,@pilt,@fpilt)", conn);
+                    cmd.Parameters.AddWithValue("@toode", Nimetus_txt.Text);
+                    cmd.Parameters.AddWithValue("@kogus", Kogus_txt.Text);
+                    cmd.Parameters.AddWithValue("@hind", Hind_txt.Text);
+                    cmd.Parameters.AddWithValue("@pilt", Nimetus_txt.Text+extension);
+
+                    imageData = File.ReadAllBytes(open.FileName);
+                    cmd.Parameters.AddWithValue("@fpilt",imageData);
+                    cmd.ExecuteNonQuery();
+
+                    conn.Close();
+                    Eemaldamine();
+                    NaitaAndmed();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Andmebaasiga viga!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sisesta andmeid!");
+            }
+        }
+        int ID=0;
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ID = (int)dataGridView1.Rows[e.RowIndex].Cells["Id"].Value;
+            Nimetus_txt.Text = dataGridView1.Rows[e.RowIndex].Cells["Nimetus"].Value.ToString();
+            Kogus_txt.Text = dataGridView1.Rows[e.RowIndex].Cells["Kogus"].Value.ToString();
+            Hind_txt.Text = dataGridView1.Rows[e.RowIndex].Cells["Hind"].Value.ToString();
+            try
+            {
+
+                using (FileStream fs = new FileStream(Path.Combine(Path.GetFullPath(@"..\..\Pildid"), dataGridView1.Rows[e.RowIndex].Cells["Pilt"].Value.ToString()), FileMode.Open, FileAccess.Read))
+                {
+                    pictureBox1.Image = Image.FromStream(fs);
+                }
+
+                //pictureBox1.Image = Image.FromFile(Path.Combine(Path.GetFullPath(@"..\..\Pildid"), dataGridView1.Rows[e.RowIndex].Cells["Pilt"].Value.ToString()));
+                pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            }
+            catch (Exception)
+            {
+                using (FileStream fs = new FileStream(Path.Combine(Path.GetFullPath(@"..\..\Pildid"), "pilt.png"), FileMode.Open, FileAccess.Read))
+                {
+                    pictureBox1.Image = Image.FromStream(fs);
+                }
+                //pictureBox1.Image = Image.FromFile(Path.Combine(Path.GetFullPath(@"..\..\Pildid"),"pilt.png"));
+                
+            }
+        }
+
+        private void uuenda_btn_Click(object sender, EventArgs e)
+        {
+            if (Nimetus_txt.Text.Trim() != string.Empty && Kogus_txt.Text.Trim() != string.Empty && Hind_txt.Text.Trim() != string.Empty)
+            {
+                try
+                {
+                    conn.Open();
+                    cmd = new SqlCommand("UPDATE Toode SET Nimetus=@toode,Kogus=@kogus,Hind=@hind,Pilt=@pilt WHERE Id=@id", conn);
+                    cmd.Parameters.AddWithValue("@id", ID);
+                    cmd.Parameters.AddWithValue("@toode", Nimetus_txt.Text);
+                    cmd.Parameters.AddWithValue("@kogus", Kogus_txt.Text);
+                    cmd.Parameters.AddWithValue("@hind", Hind_txt.Text);
+                    cmd.Parameters.AddWithValue("@pilt", Nimetus_txt.Text+extension);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    NaitaAndmed();
+                    Eemaldamine();                    
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Andmebaasiga viga!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Sisesta andmeid!");
+            }
+        }
+
+        private void Eemaldamine()
+        {
+            MessageBox.Show("Andmed edukalt uuendatud", "Uuendamie");
+            Nimetus_txt.Text = "";
+            Kogus_txt.Text = "";
+            Hind_txt.Text = "";
+            using (FileStream fs = new FileStream(Path.Combine(Path.GetFullPath(@"..\..\Pildid"), "pilt.png"), FileMode.Open, FileAccess.Read))
+            {
+                pictureBox1.Image = Image.FromStream(fs);
+            }
+            //pictureBox1.Image = Image.FromFile(Path.Combine(Path.GetFullPath(@"..\..\Pildid"), "pilt.png"));
+        }
+
+        
+        private void otsipilt_btn_Click(object sender, EventArgs e)
+        {
+            open= new OpenFileDialog();
+            open.InitialDirectory = @"C:\Users\marina.oleinik\Pictures\";
+            open.Multiselect = false;
+            open.Filter = "Images Files(*.jpeg;*.png;*.bmp;*.jpg)|*.jpeg;*.png;*.bmp;*.jpg";
+            openfile= new FileInfo(@"C:\Users\marina.oleinik\Pictures\"+open.FileName);
+            if (open.ShowDialog()==DialogResult.OK && Nimetus_txt.Text!=null)
+            { 
+
+                save= new SaveFileDialog();
+                save.InitialDirectory = Path.GetFullPath(@"..\..\Pildid");
+                
+                extension=Path.GetExtension(open.FileName);
+                
+                save.FileName= Nimetus_txt.Text + extension;
+                save.Filter = "Images" + Path.GetExtension(open.FileName) + "|" + Path.GetExtension(open.FileName);
+                if (save.ShowDialog()==DialogResult.OK && Nimetus_txt!=null)
+                {
+                    File.Copy(open.FileName, save.FileName);
+                    pictureBox1.Image=Image.FromFile(save.FileName);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Puudub toode nimetus või ole Cancel vajutatud");
+            }
+        }
+        public void Failivabastus(PictureBox pb)
+        {
+            if (pb.Image != null)
+            {
+                pb.Image.Dispose();
+                pb = null;
+            }
+        }
+        private void kustuta_btn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ID = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["Id"].Value);
+                if(ID!=0) 
+                {
+                    conn.Open();
+                    cmd = new SqlCommand("DELETE FROM Toode WHERE Id=@id", conn);
+                    cmd.Parameters.AddWithValue("@id", ID);
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    string file = dataGridView1.SelectedRows[0].Cells["Pilt"].Value.ToString();//Nimetus_txt.Text+extension;                   
+                    Eemaldamine();
+                    NaitaAndmed();
+                    System.Threading.Thread.Sleep(500);
+                    Kustuta_fail(file);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Viga!");
+            }
+        }
+        private void Kustuta_fail(string file)
+        {
+            try
+            {
+                
+                string filePath = Path.Combine(Path.GetFullPath(@"..\..\Pildid"), file);
+                MessageBox.Show($"Püüan kustutada faili: {filePath}");
+                Failivabastus(pictureBox1);
+                if (File.Exists(filePath))
+                {
+                        File.Delete(filePath);
+                        MessageBox.Show("Fail on kustutatud");
+                }
+                else
+                {
+                    MessageBox.Show("Fail ei leitud");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failiga probleemid {ex.Message}");
+            }
+        }
+        private void dataGridView1_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex ==4 )
+            {
+                imageData = dataGridView1.Rows[e.RowIndex].Cells["FusPilt"].Value as byte[];
+                if (imageData != null)
+                {
+                    using (MemoryStream ms = new MemoryStream(imageData))
+                    {
+                        Image image = Image.FromStream(ms);
+
+                        Loopilt(image, e.RowIndex);
+                    }
+                }
+            }
+        }
+        Form popupForm;
+        private void Loopilt(Image image, int r)
+        {
+            popupForm = new Form();
+            popupForm.FormBorderStyle = FormBorderStyle.None;
+            popupForm.StartPosition = FormStartPosition.Manual;
+            popupForm.Size = image.Size;
+
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Image = image;
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+
+            popupForm.Controls.Add(pictureBox);
+
+            Rectangle cellRectangle = dataGridView1.GetCellDisplayRectangle(4, r, true);
+            Point popupLocation = dataGridView1.PointToScreen(cellRectangle.Location);
+
+            popupForm.Location = new Point(popupLocation.X + cellRectangle.Width, popupLocation.Y); 
+            popupForm.Show();
+        }
+
+        private void dataGridView1_CellMouseLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            if (popupForm != null && !popupForm.IsDisposed)
+            {
+                popupForm.Close();
+            }
+        }
+    }
+}
